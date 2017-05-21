4970089ecd41f2a98652f0c68c6d012dffdfb597
Carlos Frostte <carlosfrostte@gmail.com>
2017-05-21 19:28:42 -0300

    Tambien se establecen relaciones y validaciones a los 3 modelos, modificandose el esquema de la bd

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,11 @@
 class Favorite < ApplicationRecord
+
+	belongs_to :user
+
+	validates :idApi, presence: true
+	validates :kind, presence: true
+
+	validates :user, :uniqueness => {:scope => :idApi}
+	validates :idApi, :uniqueness => {:scope => :user}
+
 end
