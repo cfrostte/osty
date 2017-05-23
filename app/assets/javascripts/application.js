dc55d6539d9d6895fb624f21e74889e64754015a
Carlos Frostte <carlosfrostte@gmail.com>
2017-05-23 14:55:11 -0300

    ...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require_tree ./bootstrap/js
 //= require jquery
 //= require jquery_ujs
+//= require_tree ./bootstrap/js
 //= require turbolinks
 //= require views
 //= require apis
