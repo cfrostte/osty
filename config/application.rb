0e66cb77b317320363bb9a9a1920205b824f7a9b
Carlos Frostte <carlosfrostte@gmail.com>
2017-05-23 15:50:26 -0300

    ...

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,6 @@ module Osty
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :es
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   end
 end
