dc17820a9d85dba224f626be931429387e6d1b1b
Carlos Frostte <carlosfrostte@gmail.com>
2017-05-23 15:46:16 -0300

    ...

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Osty
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :es
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
