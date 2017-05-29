20fe3509cce2bec2619a8f190710a0c75e1720f1
Carlos Frostte <carlosfrostte@gmail.com>
2017-05-29 20:25:56 -0300

    Se cambia la API Spotify por Last.fm

--- app/assets/javascripts/apis.js
+++ app/assets/javascripts/apis.js
@@ -1,7 +1,8 @@
 var protocol = window.location.protocol;
 var host = window.location.host;
 
-var k = "22f58faad6207b2f0dcf3068cc50bb74";
+var k_themoviedb = "22f58faad6207b2f0dcf3068cc50bb74";
+var k_lastfm = "4268ad0746656798a6616f4bbac67dd1";
 
 var response_music = null;
 var response_film = null;
@@ -12,7 +13,7 @@ var film = "<span class='glyphicon glyphicon-film'></span>";
 
 var loading = "<center><img src='/img/loading.gif' alt='loading...'></center>";
 
-$('document').ready( function() {
+$( document ).on('turbolinks:load', function() {
 
 	var input = document.getElementById("q");
 
@@ -30,16 +31,20 @@ $('document').ready( function() {
 function search(q) {
 
 	var url_collaboration = protocol+"//"+host+"/collaborations/search";
-	var url_music = "https://api.spotify.com/v1/search?query="+q+"&type=track";
-	var url_film = "https://api.themoviedb.org/3/search/movie?query="+q+"&api_key="+k+"";
+	var url_music = "http://ws.audioscrobbler.com/2.0/?method=track.search&track="+q+"&api_key="+k_lastfm+"&format=json";
+	var url_film = "https://api.themoviedb.org/3/search/movie?query="+q+"&api_key="+k_themoviedb+"";
+	
+	var queue_count = 0;
+
+	var response_music = null;
+	var response_film = null;
 
 	var settings_music = {
-		"async": true,
-		"crossDomain": true,
 		"url": url_music,
-		"method": "GET",
-		"headers": {},
-		"data": ""
+		"crossDomain": true,
+		"dataType": 'json',
+		"type": 'GET',
+		"data": "",
 	}
 
 	var settings_film = {
@@ -54,44 +59,43 @@ function search(q) {
 	$.ajax(settings_music).done(function (response) {
 		
 		response_music = response;
+		queue_count+=1;
+		
+		if (queue_count==2) call_server(response_music, response_film);
 
-		$.ajax(settings_film).done(function (response) {
-
-			response_film = response;
-			
-			var data = { "music": response_music, "film": response_film };
-
-			var settings_collaboration = {
-			    "url": url_collaboration,
-			    "type": 'POST',
-			    "data": JSON.stringify(data),
-			    "contentType": 'application/json; charset=utf-8',
-			    "dataType": 'json',
-			    "async": true,
-			    error: function(error, data) {
-			    	console.log("aca esta algo mal");
-			    },
-			    success: function(data) {
-			    	console.log("anduvo bien y devuelve", data);
-			    }
-			}
-
-			$.ajax(settings_collaboration).done(function (response) {
-				document.getElementById("found").innerHTML = "";
-				populateOsty(response);
-				console.log("response");
-				console.log(response);
-				populateSpotify(response_music);
-				console.log("response_music");
-				console.log(response_music);
-				populateThemoviedb(response_film);
-				console.log("response_film");
-				console.log(response_film);
-			});
-	
-		});
-	
 	});
+
+	$.ajax(settings_film).done(function (response) {
+		
+		response_film = response;
+		queue_count+=1;
+
+		if (queue_count==2) call_server(response_music, response_film);
+
+	});
+
+	function call_server(response_music, response_film) {
+
+		var data = { "music": response_music, "film": response_film };
+		
+		var settings_collaboration = {
+			
+			"url": url_collaboration,
+			"type": 'POST',
+			"data": "",
+			"contentType": 'application/json; charset=utf-8',
+			"dataType": 'json',
+			"async": true
+		}
+
+		$.ajax(settings_collaboration).done(function (response) {
+			document.getElementById("found").innerHTML = "";
+			populateOsty(response);
+			populateLastfm(response_music);
+			populateThemoviedb(response_film);
+		});
+
+	}
 
 }
 
@@ -122,16 +126,16 @@ function populateOsty(response) {
 
 }
 
-function populateSpotify(response) {
+function populateLastfm(response) {
 
 	var content = "";
-	var array = response.tracks.items;
+	var array = response.results.trackmatches.track;
 	var l = array.length;
 
 	for (i=0; i<l; i++) {
 
 		var it = array[i];
-		var info = it.artists[0].name+" - "+it.name;
+		var info = it.artist+" - "+it.name;
 		var osty = "#";
 		var link = "<a target='_blank' href='"+osty+"'>"+info+"</a>";
 
@@ -142,6 +146,7 @@ function populateSpotify(response) {
 	document.getElementById("found").innerHTML += content;
 
 }
+
 
 function populateThemoviedb(response) {
 
