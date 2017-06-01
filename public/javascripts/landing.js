f35e75943132154a295d3d36e2f41e9513bb1da4
Roccma <mauroroccaf@gmail.com>
2017-05-31 21:06:01 -0300

    Landing page terminada

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -11,19 +11,107 @@ $("#btn-buscar").click(function (e) {
 //$("#main").animate({ scrollTop : $("#dangerNuevaIncidencia").offset().top + 100 }, 750 );
 
 $('#quees').on('click', function(){
-	$("#main").animate({ scrollTop : $("#divWallpaper").offset().top }, 750 );
+	 $('html,body').animate({
+        scrollTop: $("#divWallpaper").offset().top - 150
+    }, 1000);
 });
 
 $('#funcionalidades').on('click', function(){
 
-	alert($("#funcionalidades").offset().top + 500);
-	$("#main").animate({ scrollTop : $("#funcionalidades").offset().top + 500}, 750 );
+	//alert($("#funcionalidades").offset().top + 500);
+	 $('html,body').animate({
+        scrollTop: $("#tituloFuncionalidades").offset().top - 150
+    }, 1000);
+	//$("body").animate({ scrollTop : $("#tituloFuncionalidades").offset().top + 500}, 750 );
 });
 
 $('#equipo').on('click', function(){
-	$("#main").animate({ scrollTop : $("#footer").offset().top }, 750 );
+	 $('html,body').animate({
+        scrollTop: $(".footerEquipo").offset().top - 150
+    }, 1000);
 });
 
 $('#contacto').on('click', function(){
-	$("#main").animate({ scrollTop : $("#footer").offset().top }, 750 );
+	 $('html,body').animate({
+        scrollTop: $(".footerContacto").offset().top - 150
+    }, 1000);
 });
+
+
+$('#queesH').on('click', function(){
+	$(".menuVertical").animate({
+            width: "toggle"
+            /*
+            width: "show"
+            width: "hide"
+            */
+        });
+	 $('html,body').animate({
+        scrollTop: $("#divWallpaper").offset().top - 150
+    }, 1000);
+});
+
+$('#funcionalidadesH').on('click', function(){
+
+	//alert($("#funcionalidades").offset().top + 500);
+	$(".menuVertical").animate({
+            width: "toggle"
+            /*
+            width: "show"
+            width: "hide"
+            */
+        });
+	 $('html,body').animate({
+        scrollTop: $("#tituloFuncionalidades").offset().top - 150
+    }, 1000);
+	//$("body").animate({ scrollTop : $("#tituloFuncionalidades").offset().top + 500}, 750 );
+});
+
+$('#equipoH').on('click', function(){
+	$(".menuVertical").animate({
+            width: "toggle"
+            /*
+            width: "show"
+            width: "hide"
+            */
+        });
+	 $('html,body').animate({
+        scrollTop: $(".footerEquipo").offset().top - 150
+    }, 1000);
+});
+
+$('#contactoH').on('click', function(){
+	$(".menuVertical").animate({
+            width: "toggle"
+            /*
+            width: "show"
+            width: "hide"
+            */
+        });
+	 $('html,body').animate({
+        scrollTop: $(".footerContacto").offset().top - 150
+    }, 1000);
+});
+
+
+$('#barrasMenuVertical').on('click', function(){
+	//alert("aca");
+	//$('.menuVertical').fadeIn();
+	$(".menuVertical").animate({
+            width: "toggle"
+            /*
+            width: "show"
+            width: "hide"
+            */
+        });
+});
+
+$('.btnCerrar').on('click', function(){
+	$(".menuVertical").animate({
+            width: "toggle"
+            /*
+            width: "show"
+            width: "hide"
+            */
+        });
+})
