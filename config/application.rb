886bf1e0293cf5106d610d1498be8223fc500020
Carlos Frostte <carlosfrostte@gmail.com>
2017-07-08 00:56:51 -0300

    ...

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Osty
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :es
-    config.assets.initialize_on_precompile = false
     config.serve_static_assets = true
   end
 end
