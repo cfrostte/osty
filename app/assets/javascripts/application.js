7f92efe46799ded6330a3fa5d4853f87d00b8dca
Carlos Frostte <carlosfrostte@gmail.com>
2017-07-08 02:36:31 -0300

    ...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/js/bootstrap.min
-//= require awesomplete
 //= require turbolinks
 //= require views
 //= require apis
--- public/aw.js
+++
@@ -1,6 +0,0 @@
-var input = document.getElementById("q");
-// console.log(input);
-var awesomplete = new Awesomplete(input);
-awesomplete.list = ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"];
-// awesomplete.list = busqueda;
-awesomplete.list = AWlist;
