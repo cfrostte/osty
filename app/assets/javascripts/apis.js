ec9da80e5ee6c20fbedb1955f596faa9f1a160ed
Carlos Frostte <carlosfrostte@gmail.com>
2017-07-11 13:41:04 -0300

    Se extiende el layout por defecto para usar el nuevo estilo (solo el menu y el listado de canciones)

--- app/assets/javascripts/apis.js
+++ app/assets/javascripts/apis.js
@@ -1,31 +1,37 @@
+/* COMPONENTES CONTEXTUALES DE LAS URL's */
 var protocol = window.location.protocol;
 var host = window.location.host;
 
+/* CLAVES REQUERIDAS PARA QUE LAS API's BRINDEN AUTORIZACION */
 var key_music = "4268ad0746656798a6616f4bbac67dd1"; //Last.fm
 var key_film = "22f58faad6207b2f0dcf3068cc50bb74"; //The Movie Database
 
+/* SEMI-URL's CON LAS CUALES SE REALIZAN LAS PETICIONES AJAX, QUE INCLUYEN LAS CLAVES DE AUTENTICACION */
+var sub_url_music = "https://ws.audioscrobbler.com/2.0/?method=track.search&format=json&api_key="+key_music;
+var sub_url_film = "https://api.themoviedb.org/3/search/movie?api_key="+key_film;
+
+/* GIF ANIMADO QUE INDICA QUE LA PAGINA ESTA PROCESANDO ALGO */
+var loading = "<center><img src='/img/loading.gif' alt='loading...'></center>";
+
+/* ARREGLOS QUE ALMACENAN LA INFORMACION DE LOS ELEMENTOS LISTADOS */
 var array_collaboration = null;
 var array_music = null;
 var array_film = null;
 
+/* ICONOS QUE RESPRESENTAN EL TIPO DE ELEMENTO LISTADO */
 var collaboration = "<span class='glyphicon glyphicon-play-circle'></span>";
 var music = "<span class='glyphicon glyphicon-music'></span>";
 var film = "<span class='glyphicon glyphicon-film'></span>";
 
-var loading = "<center><img src='/img/loading.gif' alt='loading...'></center>";
-
-var sub_url_music = "https://ws.audioscrobbler.com/2.0/?method=track.search&format=json&api_key="+key_music;
-var sub_url_film = "https://api.themoviedb.org/3/search/movie?api_key="+key_film;
-
-var public_query = null;
-var public_page = null;
-
-var type_from_to_modal = null;
-var item_from_to_modal = null;
-var array_to_songs = null;
-var array_to_movies = null;
-var checked_items = null;
-var listener_atached_for_modal = false;
+/* VARIABLES COMPARTIDAS POR FUNCIONES */
+var public_query = null; // Termino de busqueda
+var public_page = null; // Pagina actual
+var type_from_to_modal = null; // Si es una cancion, se busca peliculas (y viceversa)
+var item_from_to_modal = null; // Elemento desde el cual se colaborara con otros
+var array_to_songs = null; // Canciones con las cuales relacionar una pelicula
+var array_to_movies = null; // Peliculas con las cuales relacionar una cancion
+var checked_items = null; // Elementos seleccionados con los cuales enlazar otro
+var listener_attached_for_modal = false; // Ya se adjunto un evento para mostrar el modal?
 
 // var AWlist = ["123124", "1231221Java", "656J65av6aScript", "65646Brainfuck", "qwertLOLCODE", "treNode.js", "zzzRuby on Rails",];
 var AWlist = [];
@@ -33,10 +39,10 @@ var AWlist = [];
 var busqueda = [];
 
 if (!!localStorage.getItem('busqueda')) {
-	console.log("Existe local");
+	// console.log("Existe local");
 	AWlist = localStorage.getItem('busqueda').split(",");
 } else {
-	console.log("No existe local");
+	// console.log("No existe local");
 	localStorage.setItem('busqueda',AWlist);
 }
 
@@ -44,6 +50,7 @@ var check = function(needle) {
     
     // Per spec, the way to identify NaN is that it is not equal to itself
     var findNaN = needle !== needle;
+    
     var indexOf;
 
     if (!findNaN && typeof Array.prototype.indexOf === 'function') {
@@ -76,11 +83,14 @@ var check = function(needle) {
 
 };
 
+/* EN RAILS, EL CONTENIDO ESTA REALMENTE LISTO CUANDO TURBOLINKS LO ESTA */
 $( document ).on('turbolinks:load', function() {
 
 	var input_q = document.getElementById("q");
 	var doneTypingInterval = 1000;
 	var typingTimer;
+
+	/* ASIGNACION DE ESCUCHA DE EVENTOS PARA BUSQUEDA Y SUGERENCIAS */
 
 	if (input_q) input_q.addEventListener("change", function () {
 		
@@ -100,7 +110,7 @@ $( document ).on('turbolinks:load', function() {
 			clearTimeout(typingTimer);
 			typingTimer = setTimeout(doneTyping, doneTypingInterval);		
 		} else {
-			console.log("Es flecha");
+			// console.log("Es flecha");
 		}
 	
 	});
@@ -111,8 +121,8 @@ $( document ).on('turbolinks:load', function() {
 
 	function doneTyping() {
 		
-		console.log("Entro en el search");
-		console.log("valor="+input_q.value);
+		// console.log("Entro en el search");
+		// console.log("valor="+input_q.value);
 		
 		if (input_q.value) {
 
@@ -135,7 +145,7 @@ $( document ).on('turbolinks:load', function() {
 
 				if (AWlist.length<100 && agregar) {
 					AWlist.unshift(input_q.value);
-					console.log("Se agrega");
+					// console.log("Se agrega");
 				}
 
 				if (AWlist.length==100 && agregar){
@@ -153,6 +163,7 @@ $( document ).on('turbolinks:load', function() {
 
 });
 
+/* ESTABLECE CONEXIONES CON ESTA APLICACION Y LAS DOS API's PARA PODER BUSCAR */
 function search(query, page) {
 
 	document.getElementById("found").innerHTML = loading;
@@ -160,19 +171,21 @@ function search(query, page) {
 	public_query = query;
 	public_page = page;
 
+	/* SE CONSTRUYEN LAS URL's FINALES CON LAS CUALES COMUNICARSE */
 	var url_collaboration = protocol+"//"+host+"/collaborations/search";
 	var url_music = sub_url_music+"&track="+query+"&page="+page;
 	var url_film = sub_url_film+"&query="+query+"&page="+page;
-
 	var url_check_favorites = protocol+"//"+host+"/favorites/check";
 	
+	/* VARIABLES EN LAS CUALES SE ALMACENARAN LAS RESPUESTAS A LAS PETICIONES */
 	var response_collaboration = null;
 	var response_music = null;
 	var response_film = null;
 
-	var queue_count = 0;
+	var queue_count = 0; // Se incrementa conforme los servidores responden
+	var total = 3; // Indica el total de conexiones a establecer con los servidores
 
-	var total = 3;
+	/* CONFIGURACIONES PARA CADA UNA DE LAS PETICIONES A REALIZAR */
 
 	var settings_collaboration = {
 		"async": true,
@@ -199,6 +212,8 @@ function search(query, page) {
 		"method": "GET",
 		"url": url_film,
 	}
+
+	/* COMIENZAS LAS PETICIONES ASINCRONAS HACIA LOS SERVIDORES */
 
 	$.ajax(settings_collaboration).done(function (response) {
 		
@@ -227,6 +242,19 @@ function search(query, page) {
 
 	});
 
+	/*
+	
+	ESTA ES LA ULTIMA LLAMADA REALIZADA A UN SERVIDOR
+	-------------------------------------------------
+	NECESITA ESPERAR A QUE TERMINEN TODAS LAS LLAMADAS ANTERIORES,
+	PUES ESTA DEBE ENVIAR TODOS LOS DATOS PARA PROCESARLOS.
+
+	SE ENTIENDE POR PROCESAR LOS DATOS A:
+		1) NORMALIZAR LOS NOMBRES DE LOS ATRIBUTOS CONFORME LOS MDOELOS
+		2) CONSTRUIR URL's DE IMAGENES DE OBJETOS (CANCIONES O PELICULAS)
+		3) VERIFICAR PARA CADA UNO DE ELLOS, SI ESTA O NO EN FAVORITOS
+	
+	*/
 	function final_call() {
 
 		var data = {
@@ -244,28 +272,29 @@ function search(query, page) {
 
 		$.ajax(settings_check_favorites).done(function (response) {
 			
+			/* VARIABLES PARA LA PAGINACION DE RESULTADOS */
 			document.getElementById("found").innerHTML = "";
 			document.getElementById("actual").innerHTML = "";
 			document.getElementById("pages").innerHTML = "";
 
+			/* CUALES DE DE LOS SIGUIENTES TIPOS DE ITEMS TIENEN RESULTADOS? */
 			var found_collaborations = false;
 			var found_music = false;
 			var found_film = false;
 
+			/* SE RESPONDE A LA PREGUNTA ANTERIOR Y AL MISMO TIEMPO SE MUESTRAN */
 			found_collaborations = populateCollaboration(response.collaboration);
 			found_music = populateMusic(response.music);
 			found_film = populateFilm(response.film);
 
+			/* SE DA LA OPCION DE VOLVER A LA PAGINA ACTUAL (SI CORRESPONDE) */
 			if (public_page>1 && (found_collaborations||found_music||found_film)) {
-				
 				document.getElementById("actual").innerHTML = "<hr>Página "+public_page+"<hr>";
-			
 			} else {
-			
 				document.getElementById("actual").innerHTML = "<hr>";
-			
 			}
 
+			/* REALIZA LA PAGINACION, SOLAMENTE SI EXISTEN RESULTADOS */
 			if (found_collaborations||found_music||found_film) paginate();
 		
 		});
@@ -273,6 +302,17 @@ function search(query, page) {
 	}
 
 }
+
+/*
+
+EN FUNCION DE LAS RESPUESTAS DEL SERVIDOR MUESTRAN LOS ELEMENTOS
+EN EL LISTADO DE RESULTADOS, CADA FUNCION RESPONDE TRUE SI MOSTRO ALGO.
+
+EL CONTENIDO SE CONSTRUYE CONCETENANDO UNA VARIABLE content Y A SU VEZ
+ESTA VARIABLE SE CONCATENA CON EL NODO HTML id=found
+CADA UNA DE ESTAS FUNCIONES HACE USO DE ESTE ULTIMO
+
+*/
 
 function populateCollaboration(response) {
 
@@ -316,17 +356,48 @@ function populateMusic(response) {
 		var href = "https://open.spotify.com/search/songs/"+info+"";
 		var link = "<a target='_blank' href='"+href+"'>"+info+"</a>";
 
-		var cooperate = "<span onclick='chooseFrom(this, "+i+")'"+
-		"type='song' class='modal_song glyphicon glyphicon-send'></span>";
+		/*
+		CUANDO SE HACE CLIC EN COLABORAR DESDE ESTA CANCION,
+		SE PASA EL NODO HTML Y SE PASA EL INDICE DEL OBJETO DEL array_music
+		QUE CONTIENE LA INFORMACION YA PROCESADA POR EL SERVIDOR
+		*/
+		// var cooperate = "<span onclick='chooseFrom(this, "+i+")'"+
+		// "type='song' class='modal_song glyphicon glyphicon-send'></span>";
 
-		var star = "glyphicon glyphicon-star-empty";
+		var collaborate = "<span onclick='chooseFrom(this, "+i+")'"
+		+" type='song' class='modal_song fa fa-handshake-o spanCollaboration'"
+		+" style = 'font-size: 45px; margin-top: 15px; margin-left: 200px'></span>";
 
-		if (it.favorited) star = "glyphicon glyphicon-star";
+		var star = "fa fa-star spanFavourite"; // Estrella vacia
 
-		var add = "<span onclick='addToFavorites(this, "+i+")'"+
-		"type='song' class='"+star+"'></span>";
+		/* SI ESTA EN FAVORITOS, ENTONCES LA ESTRELLA SE CAMBIA A LLENA */
+		if (it.favorited) star += " estrellaFavorito";
 
-		content += "<p>"+music+" "+link+" "+cooperate+" "+add+"</p>";
+		/*
+		CUANDO SE HACE CLIC EN AGREGAR ESTA CANCION A FAVORITOS,
+		SE PASA EL NODO HTML Y SE PASA EL INDICE DEL OBJETO DEL array_music
+		QUE CONTIENE LA INFORMACION YA PROCESADA POR EL SERVIDOR
+		*/
+		// var add = "<span onclick='addToFavorites(this, "+i+")'"+
+		// "type='song' class='"+star+"'></span>";
+
+		var add = "<span onclick='addToFavorites(this, "+i+")'"
+		+" type='song' class='"+star+"' style='margin-left: 10px'></span>";
+
+		// content += "<p>"+music+" "+link+" "+cooperate+" "+add+"</p>";
+		
+		content += "<div class = 'musicContainer' style = 'width: 100%; height : 250px'>" + 
+						"<img src = '" + it.img_url + "' style = 'width: 130px; height: 130px; margin: 50px 0; display: inline-block'/>" + 
+						"<div style = 'width: 50%; display: inline-block; top: 30px; left: 20px; position: relative;'>" +
+							"<p style = 'font-size: 22px; color: #A4A4A4'>" + it.name + add + "</p><p style = 'font-size: 17px; color: white'>" + it.artist + "</p>" +
+							"<img src = '/img/iconoMusica.png' style = 'width: 50px; height: 50px; margin-top: 9px'/>" +
+						"</div>" +
+						"<div style = 'font-size: 17.5px; color: white; width: 50%; position: relative; top: -139px; left: 579px'>" +
+							"Escuchar:<br>" +
+							"<a href = '" + href + "' class = 'linkSpotify' target = '_blank'><span class = 'fa fa-spotify' style = 'font-size: 45px; margin-top: 15px'></span></a>&nbsp;&nbsp;<a href = '" + it.url + "' class = 'linkLastFM' target = '_blank'><span class = 'fa fa-lastfm-square' style = 'font-size: 45px; margin-top: 15px'></span></a>" +
+							collaborate + 
+						"</div>" +
+					"</div>";
 			
 	}
 
@@ -351,13 +422,24 @@ function populateFilm(response) {
 		var href = "http://www.imdb.com/find?&q="+it.name+"&s=tt";
 		var link = "<a target='_blank' href='"+href+"'>"+info+"</a>";
 
+		/*
+		CUANDO SE HACE CLIC EN COLABORAR DESDE ESTA PELICULA,
+		SE PASA EL NODO HTML Y SE PASA EL INDICE DEL OBJETO DEL array_film
+		QUE CONTIENE LA INFORMACION YA PROCESADA POR EL SERVIDOR
+		*/
 		var cooperate = "<span onclick='chooseFrom(this, "+i+")'"+
 		"type='movie' class='glyphicon glyphicon-send'></span>";
 
-		var star = "glyphicon glyphicon-star-empty";
+		var star = "glyphicon glyphicon-star-empty"; // Estrella vacia
 
+		/* SI ESTA EN FAVORITOS, ENTONCES LA ESTRELLA SE CAMBIA A LLENA */
 		if (it.favorited) star = "glyphicon glyphicon-star";
 
+		/*
+		CUANDO SE HACE CLIC EN AGREGAR ESTA PELICULA A FAVORITOS,
+		SE PASA EL NODO HTML Y SE PASA EL INDICE DEL OBJETO DEL array_film
+		QUE CONTIENE LA INFORMACION YA PROCESADA POR EL SERVIDOR
+		*/
 		var add = "<span onclick='addToFavorites(this, "+i+")'"+
 		"type='movie' class='"+star+"'></span>";
 
@@ -371,11 +453,16 @@ function populateFilm(response) {
 
 }
 
+/******************************************************************************/
+
+/* ESTABLECE UNA CONEXION AL SERVIDOR PARA AGREGAR/QUITAR EL ELEMENTO A FAVORITOS */
 function addToFavorites(which, i) {
 
-	var item = null;
-	var type = which.getAttribute('type');
+	var item = null; // A definir mas adelante en funcion de su tipo
+	var type = which.getAttribute('type'); // El tipo de elemento que se clickeo
 	
+	/* BUSCAN LOS ELEMENTOS EN LOS ARREGLOS GLOBALES EN FUNCION DE SU TIPO */
+
 	if (type=='song') {
 		item = array_music[i];
 	}
@@ -384,15 +471,20 @@ function addToFavorites(which, i) {
 		item = array_film[i];
 	}
 
+	/* CONSTRUYE LA INFORMACION A ENVIAR AL SERVIDOR */
+
 	var json = {
 		"item" : item,
 		"type" : type,
 	};
 	
+	/* PROCESA LA INFORMACION CONSTRUIDA */
 	var data = JSON.stringify(json);
 
+	/* CONSTRUYE LA URL CON LA CUAL ESTABLECER LA COMUNICACION */
 	var url = protocol+"//"+host+"/favorites/add";
 
+	/* CONFIGURA DE QUE MANERA SE ESTABLECERA LA CONEXION */
 	var settings = {
 		"async": true,
 		"contentType": 'application/json; charset=utf-8',
@@ -404,17 +496,34 @@ function addToFavorites(which, i) {
 
 	$.ajax(settings).done(function (response) {
 
-		if (response.added) which.setAttribute('class','glyphicon glyphicon-star');
-		else which.setAttribute('class','glyphicon glyphicon-star-empty');
+		/* SI EL USUARIO ESTA LOGUEADO, MODIFICA LA ESTRELLA DEL NODO HTML SEGUN LA RESPUESTA */
+		if (response.added) which.setAttribute('class','fa fa-star spanFavourite estrellaFavorito');
+		else which.setAttribute('class','fa fa-star spanFavourite');
 	
 	});
 
 }
 
-function chooseFrom(which, i) {
-	
-	type_from_to_modal = which.getAttribute('type');
+/*
 
+ABRE UNA VENTANA MODAL DESDE LA CUAL SE BUSCARAN ELEMENTOS CON LOS CUALES
+RELACIONAR AL ELEMENTO DESDE EL CUAL SE ESTA HACIENDO CLIC.
+
+EL SERVIDOR AL RECIBIR LOS ELEMENTOS, CREARA UNA COLABORACION CON CADA UNO,
+EN DONDE UNO DE LOS ELEMENTOS (EN EL QUE SE HIZO CLIC) ES SIEMPRE EL MISMO.
+
+*/
+function chooseFrom(which, i) {
+
+	type_from_to_modal = which.getAttribute('type'); // Tipo en el que se hizo clic
+
+	/*
+
+	BUSCAN LOS ELEMENTOS QUE CONTIENEN LA INFORMACION,
+	EN FUNCION DEL TIPO DEL NODO HTML EN EL CUAL SE CLICKEO
+
+	*/
+	
 	if (type_from_to_modal=='song') {
 		item_from_to_modal = array_music[i];
 
@@ -444,8 +553,10 @@ function chooseFrom(which, i) {
 	
 	}
 
+	/* INPUT EN DONDE SE INGRESARA EL TERMINO DE BUSQUEDA DE ELEMENTOS */
 	var modal_q = document.getElementById('modal_q');
 
+	/* CUANDO CAMBIA, SE COMIENZA A BUSCAR */
 	modal_q.addEventListener("change", function () {
 
 		if (modal_q.value) {
@@ -454,12 +565,15 @@ function chooseFrom(which, i) {
 
 	});
 
+	/* BOTON CON EL CUAL REALIZAR LAS COLABORACIONES */
 	var collaborate = document.getElementById("collaborate");
 
-	if (!listener_atached_for_modal) {
+	/* SI NO EXISTE UN EVENTO DE ESCUCHA PARA EL BOTON... */
+	if (!listener_attached_for_modal) {
 		
 		collaborate.addEventListener("click", function() {
 			
+			/* SE TOMAN LOS ELEMENTOS SELECCIONADOS 'CONTRARIOS' AL CLICKEADO */
 			if (type_from_to_modal=='song') checked_items = getSelectedMovies();
 			if (type_from_to_modal=='movie') checked_items = getSelectedSongs();
 
@@ -470,7 +584,7 @@ function chooseFrom(which, i) {
 				alert('Elige al menos un item con el cual relacionar');
 			}
 
-			listener_atached_for_modal = true;
+			listener_attached_for_modal = true; // Ahora si tiene escucha
 
 		});
 	
@@ -480,12 +594,14 @@ function chooseFrom(which, i) {
 
 }
 
+/* REALIZA UNA BUSQUEDA DE ITEMS 'CONTRARIOS' AL CUAL SE HIZO CLIC */
 function modalSearch(query) {
 
 	document.getElementById("modal_found").innerHTML = loading;
 
 	var url_music = sub_url_music+"&track="+query;
 	var url_film = sub_url_film+"&query="+query;
+	
 	var settings = null;
 
 	var settings_music = {
@@ -540,6 +656,13 @@ function modalSearch(query) {
 	});
 
 }
+
+/*
+
+OBTIENEN TODOS LOS ELEMENTOS SELECCIONADOS CON DETERMINADA CLASE,
+Y LOS PROCESA PARA SER COMPRENSIBLES POR EL SERVIDOR
+
+*/
 
 function getSelectedMovies() {
 
@@ -612,6 +735,10 @@ function getSelectedSongs() {
 
 }
 
+/******************************************************************************/
+
+/* LLENAN EL MODAL CON LOS RESULTADOS OBTENIDOS DE LAS API's */
+
 function modalPopulateFilm(response, this_song) {
 
 	var content = "";
@@ -654,6 +781,9 @@ function modalPopulateMusic(response, this_movie) {
 
 }
 
+/******************************************************************************/
+
+/* ESTABLECE UNA CONEXION AL SERVIDOR Y REALIZA TODAS LAS COLABORACIONES */
 function collaborateFrom(from_this_item, type, to_this_items) {
 	
 	var url = null;
@@ -712,6 +842,46 @@ function collaborateFrom(from_this_item, type, to_this_items) {
 
 }
 
+/* SISTEMA DE PAGINACION */
+
+function paginate() {
+
+	document.getElementById("q").value = public_query;
+
+	var	p = "<li class='col-md-4'><a href='' onclick='previous()'>Anterior</a></li>";
+	var	f = "<li class='col-md-4'><a href='' onclick='first()'>Pagina inicial</a></li>";
+	var n = "<li class='col-md-4'><a href='' onclick='next()'>Siguiente</a></li>";
+
+	document.getElementById("pages").innerHTML = "<hr>"+p+f+n;
+
+}
+
+function previous() {
+
+	var previous_page = public_page;
+	
+	if (public_page > 1) previous_page -= 1;
+
+	return search(public_query, previous_page);
+
+}
+
+function first() {
+	return search(public_query, 1);
+}
+
+function next() {
+	
+	var next_page = public_page + 1;
+	
+	return search(public_query, next_page);
+
+}
+
+/******************************************************************************/
+
+/* OBTIENEN DATOS FALSOS GENERADOS CON NUMEROS RANDOMICOS */
+
 function random_movies(n) {
 
 	movies = [];
@@ -749,39 +919,5 @@ function getMovies() {
 		}];
 
 	JSON.stringify(items);
-
-}
-
-function paginate() {
-
-	document.getElementById("q").value = public_query;
-
-	var	p = "<li class='col-md-4'><a href='' onclick='previous()'>Anterior</a></li>";
-	var	f = "<li class='col-md-4'><a href='' onclick='first()'>Pagina inicial</a></li>";
-	var n = "<li class='col-md-4'><a href='' onclick='next()'>Siguiente</a></li>";
-
-	document.getElementById("pages").innerHTML = "<hr>"+p+f+n;
-
-}
-
-function previous() {
-
-	var previous_page = public_page;
-	
-	if (public_page > 1) previous_page -= 1;
-
-	return search(public_query, previous_page);
-
-}
-
-function first() {
-	return search(public_query, 1);
-}
-
-function next() {
-	
-	var next_page = public_page + 1;
-	
-	return search(public_query, next_page);
 
 }
